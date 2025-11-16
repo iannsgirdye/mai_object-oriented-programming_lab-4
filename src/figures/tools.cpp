74d831b25517e6640b7ce080e4bd2da99c243ced
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2025-11-16 23:07:45 +0300

    Add "square" for Rhomb

--- src/figures/tools.cpp
+++ src/figures/tools.cpp
@@ -8,3 +8,10 @@ double side(Point<T> p1, Point<T> p2) {
   const double sideY = static_cast<double>(p1.y - p2.y);
   return (sqrt(sideX * sideX + sideY * sideY));
 }
+
+template <Scalar T>
+double Rhomb<T>::square() const {
+  const double d1 = side(*points[0], *points[2]);
+  const double d2 = side(*points[1], *points[3]);
+  return d1 * d2 / 2.0;
+}
