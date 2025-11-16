d7195aff6b16ab0ec24b130b482c44d4820f119d
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2025-11-16 22:18:49 +0300

    Add interfaces of figures

---
+++ include/figures.hpp
@@ -0,0 +1,64 @@
+#ifndef FIGURE_HPP
+#define FIGURE_HPP
+
+#include <vector>
+#include <memory>
+#include <iostream>
+#include "./point.hpp"
+
+template <Scalar T>
+class Figure {
+  protected:
+    std::vector<std::unique_ptr<Point<T>>> points;
+
+    Figure();
+    Figure(const std::vector<Point<T>> &_points);
+  
+  public:
+    virtual ~Figure() = default;
+
+    virtual double square() const = 0;
+    virtual Point<T> center() const = 0;
+
+    Figure<T> &operator=(const Figure<T> &other);
+    Figure<T> &operator=(Figure<T> &&other) noexcept;
+
+    friend std::ostream &operator<<(std::ostream &out, const Figure<T> &f);
+    friend std::istream &operator>>(std::istream &in, Figure<T> &f);
+    friend bool operator==(const Figure<T> &l, const Figure<T> &r);
+
+    class EmptyFigureException {};
+    class IncorrectPointsCountException {};
+};
+
+template <Scalar T>
+class Rectangle: public Figure<T> {
+  public:
+    Rectangle();
+    Rectangle(const std::vector<Point<T>> &ps);
+
+    double square() const override;
+    Point<T> center() const override;
+};
+
+template <Scalar T>
+class Trapezoid: public Figure<T> {
+  public:
+    Trapezoid();
+    Trapezoid(const std::vector<Point<T>> &ps);
+
+    double square() const override;
+    Point<T> center() const override;
+};
+
+template <Scalar T>
+class Rhomb: public Figure<T> {
+  public:
+    Rhomb();
+    Rhomb(const std::vector<Point<T>> &ps);
+
+    double square() const override;
+    Point<T> center() const override;
+};
+
+#endif
